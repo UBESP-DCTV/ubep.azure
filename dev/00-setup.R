a371a0b1500e385e22d8e32ed0cba2bf82f97262
Corrado Lanera <corrado.lanera@gmail.com>
2023-05-31 11:03:10 +0200

    Added GH support and CoC

--- dev/00-setup.R
+++ dev/00-setup.R
@@ -14,7 +14,10 @@ usethis::use_testthat()
 usethis::use_roxygen_md()
 usethis::use_r("foo") # delete foo.R file
 usethis::use_package_doc()
+usethis::use_code_of_conduct("corrado.lanera@ubep.unipd.it")
 usethis::use_mit_license()
 
 usethis::use_lifecycle_badge("experimental")
 usethis::use_cran_badge()
+
+usethis::use_tidy_github()
