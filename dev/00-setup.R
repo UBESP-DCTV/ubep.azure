63f831ac508440ed9c9b027e09f654a213759713
Corrado Lanera <corrado.lanera@gmail.com>
2023-05-31 10:57:49 +0200

    base setup

---
+++ dev/00-setup.R
@@ -0,0 +1,20 @@
+c(
+  "devtools", "testthat", "checkmate", "tidyverse", "withr", "fs",
+  "here", "glue", "rmarkdown", "knitr"
+) |>
+  install.packages()
+
+renv::status()
+renv::snapshot()
+
+usethis::use_description()
+usethis::use_readme_rmd()
+usethis::use_news_md()
+usethis::use_testthat()
+usethis::use_roxygen_md()
+usethis::use_r("foo") # delete foo.R file
+usethis::use_package_doc()
+usethis::use_mit_license()
+
+usethis::use_lifecycle_badge("experimental")
+usethis::use_cran_badge()
